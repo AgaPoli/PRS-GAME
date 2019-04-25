6dd631a6f5a6c012e6d1c6b9724131a1c2d10c89
Agnieszka Polichnowska <apolichnowska@tlen.pl>
2019-04-25 14:54:19 +0200

    Update HTML with modal and JS whth const

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const playerNameElement = document.getElementById("js-newGameElement");
+const newGameButton = document.getElementById("js-newGameButton");
+const playerPickElement = document.getElementById("js-playerPickElement");
+const resultsTableElement = document.getElementById("js-resultsTableElement");
+const playerPoints = document.getElementById("js-playerPoints");
+const playerName = document.getElementById("js-playerName")
+const computerPoints = document.getElementById("js-computerPoints");
+const computerName= document.getElementById("js-computerName");
+const playerPick = document.getElementById("js-playerPick");
+const computerPick = document.getElementById("js-computerPick");
+const playerPoint = document.getElementById("js-playerPoint");
+const computerPoint = document.getElementById("js-computerPoint");
+
+// new game (welcome prompt with question how many games and the name)
+
+
+
+
+
+
+
+<div class="container"  id="js-newGameElement">
+      <div class="row text-center">
+        <button class="btn btn-primary btn-md" id="js-newGameButton" data-toggle="modal" data-target="#myModal">
+          NEW GAME
+        </button>
+      </div>
+    </div>
