6a20f93d476a4cb2963dd6185a13f631692b193b
Agnieszka Polichnowska <apolichnowska@tlen.pl>
2019-04-30 15:31:33 +0200

    Create judge function

--- js/script.js
+++ js/script.js
@@ -6,14 +6,18 @@ const playerPoints = document.getElementById("js-playerPoints");
 const playerName = document.getElementById("js-playerName");
 const computerPoints = document.getElementById("js-computerPoints");
 const computerName = document.getElementById("js-computerName");
-const playerPick = document.getElementById("js-playerPick");
-const computerPick = document.getElementById("js-computerPick");
+const playerChoice = document.getElementById("js-playerPick");
+const computerChoice = document.getElementById("js-computerPick");
 const playerPoint = document.getElementById("js-playerPoint");
 const computerPoint = document.getElementById("js-computerPoint");
 const userNameElement = document.getElementById("js-userNameElement");
 const startGameBtn = document.getElementById("js-startGameBtn");
 // new game (welcome prompt with question how many games and the name)
-
+let scoreBoard = {
+  numberOfGames: 0,
+  scorePlayer: 0,
+  scoreComputer: 0
+};
 //Enter your name (assumptions for the name)
 function getUserName() {
   return userNameElement.value;
@@ -59,10 +63,51 @@ startGameBtn.addEventListener("click", event => newGame());
 
 // player choice eventlistner, function 
 
-playerPickElement.addEventListener("click", event => playerChoice());
 
-function playerChoice (target){
-  const targetBtn = target.closest("btn.");
-  const playerChoice = targetBtn.dataset.choice; 
+
+playerPickElement.addEventListener("click", event =>
+  playerChoose(event.target)
+);
+
+
+function playerChoose(target) {
+  //element to nasz target
+  const targetBtn = target.closest(".btn"); //jak naciskam na ikonke w przycisku to szuka najblizszego rodzica .btn;
+  const playerChoice = targetBtn.dataset.selection; //ma byc na cala ikone nie tylko na nazwe(papier)ma byc kolko i znaczek
+  // console.log(targetBtn, typeof playerChoice, typeof computerPick()); //playerChoice- wyswietli co wybral gracz
+  judge (playerChoice, computerPick());
+}
+
+
+function computerPick (){
+  const options =["papier", "Rock", "Scissors"];
+  return options[ Math.floor(Math.random() * 3)];
 
 }
+
+
+function judge (playerPick, computerPick) {
+  if (computerPick == playerPick) {
+    return 0; //remis
+  }
+  let winner = "";
+  if (
+    (computerPick == "Papier"   &&  playerPick == "Scissors" )||
+    (computerPick == "Rock"     &&  playerPick == "Papier" )||
+    (computerPick == "Scissors" &&  playerPick == "Rock" )
+  ) {
+    scoreBoard.scorePlayer++;
+    let winner = "player";
+  } else {
+    scoreBoard.scoreComputer++;
+    let winner = "computer";
+  }
+  result(winner);
+
+  playerChoice.innerHTML = playerPick;
+  computerChoice.innerHTML = computerPick;
+  scoreBoard.numberOfGames++;
+}
+
+
+ 
