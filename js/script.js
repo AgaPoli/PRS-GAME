03246924d153f628434d4b4a858f5b1ffcf5610a
Agnieszka Polichnowska <apolichnowska@tlen.pl>
2019-05-06 12:24:47 +0200

    Update js: modal, endGame(),result()

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const playerNameElement = document.getElementById("js-newGameElement");
+
 const newGameButton = document.getElementById("js-newGameButton");
 const playerPickElement = document.getElementById("js-playerPickElement");
 const resultsTableElement = document.getElementById("js-resultsTableElement");
 const playerPoints = document.getElementById("js-playerPoints");
-const playerName = document.getElementById("js-playerName");
+const playerNameElement = document.getElementById("js-playerName");
 const computerPoints = document.getElementById("js-computerPoints");
 const computerName = document.getElementById("js-computerName");
 const playerChoice = document.getElementById("js-playerPick");
@@ -12,12 +12,23 @@ const playerPoint = document.getElementById("js-playerPoint");
 const computerPoint = document.getElementById("js-computerPoint");
 const userNameElement = document.getElementById("js-userNameElement");
 const startGameBtn = document.getElementById("js-startGameBtn");
+const usernameElement = document.getElementById("js-username");
+const numberOfGamesElement = document.getElementById("js-numberOfGames");
+// Results Table
+const resultsTableBodyElement = document.getElementById("js-resultsTableBody");
+
 // new game (welcome prompt with question how many games and the name)
 let scoreBoard = {
   numberOfGames: 0,
   scorePlayer: 0,
   scoreComputer: 0
 };
+
+const  gameRules = {
+  numberOfGames: 3
+};
+
+let courseOfTheGame = [];
 //Enter your name (assumptions for the name)
 function getUserName() {
   return userNameElement.value;
@@ -51,12 +62,17 @@ function validateUserName(name) {
 function newGame() {
   const name = getUserName();
   if (!validateUserName(name)) {
-    alert("imie nie poprawne");
+    alert("name not correct");
 
     return;
   }
 
  $("#myModal").modal("hide");
+ gameRules.numberOfGames = numberOfGamesElement.value;
+  newGameButton.classList.add("hidden");
+  playerNameElement.innerHTML = name;
+  playerPickElement.classList.remove("hidden");
+  resultsTableElement.classList.remove("hidden");
 }
 
 startGameBtn.addEventListener("click", event => newGame());
@@ -80,7 +96,7 @@ function playerChoose(target) {
 
 
 function computerPick (){
-  const options =["papier", "Rock", "Scissors"];
+  const options =["Papier", "Rock", "Scissors"];
   return options[ Math.floor(Math.random() * 3)];
 
 }
@@ -107,7 +123,72 @@ function judge (playerPick, computerPick) {
   playerChoice.innerHTML = playerPick;
   computerChoice.innerHTML = computerPick;
   scoreBoard.numberOfGames++;
+
+const courseOfTheRound = {
+  scorePlayer: scoreBoard.scorePlayer,
+  scoreComputer: scoreBoard.scoreComputer,
+  playerMove: playerPick.toString(),
+  computerMove: computerPick.toString(),
+  won: winner
+};
+courseOfTheGame.push(courseOfTheRound); // adding an element to the end of the array
+ 
+ showResult(courseOfTheRound);
+ console.log(scoreBoard.namberOfGames);
+
+
+courseOfTheGame.forEach(elemet =>{
+  computerChoice.log(element)
+});
+}
+ 
+// The number of js-playerPoints is to be used for the name
+ function result(winner, reset = false){
+   playerPoints.innerHTML = reset ? "0": scoreBoard.scorePlayer.toString();
+   computerPoints.innerHTML = reset ? "0": scoreBoard.scoreComputer.toString();
+
+   playerPoint.innerHTML =
+    winner == "player" ? '<i class="fa fa-smile-o" aria-hidden="true"></i>' : "";
+
+  computerPoint.innerHTML =
+    winner == "computer" ? '<i class="fa fa-smile-o" aria-hidden="true"></i>' : "";
+ 
+}
+
+function endGame(){
+  newGameButton.classList.remove("hidden");
+  playerNameElement.innerHTML = "PlayerName";
+  playerPickElement.classList.add("hidden");
+  resultsTableElement.classList.add("hidden");
+  playerChoice.innerHTML = "";
+  computerChoice.innerHTML = "";
+  result("", true);
+
+  courseOfTheGame = [];
+  resultsTableBodyElement.innerHTML = "";
+
+  let scoreBoard = {
+  numberOfGames: 0,
+  scorePlayer: 0,
+  scoreComputer: 0
+};
 }
 
 
- 
+
+function showResult(courseOfTheRound) {
+  const row = `
+    <tr>
+      <th scope="row">${scoreBoard.numberOfGames}</th>
+      <td>${courseOfTheRound.playerPoint}</td>
+      <td>${courseOfTheRound. computerPoint}</td>
+      <td>${courseOfTheRound.playerMove}</td>
+      <td>${courseOfTheRound.computerMove}</td>
+      <td>${courseOfTheRound.won}</td>
+    </tr>
+  `;
+
+  resultsTableBodyElement.innerHTML = resultsTableBodyElement.innerHTML + row;
+}
+
+$("#resultModal").on("hide.bs.modal", endGame);
